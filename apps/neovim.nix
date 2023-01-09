181bc22869fa34c2c34e5a2a7efc430006667dde
Denis Lavrushko <denis.lavrushko@iits-consulting.de>
2023-01-09 16:02:33 +0100

    neovim config sort

--- apps/neovim.nix
+++ apps/neovim.nix
@@ -4,15 +4,15 @@
   programs.neovim = {
     enable = true;
     extraConfig = ''
-      set shiftwidth=2
-      set tabstop=2
-      set expandtab
-      set number
-      set clipboard=unnamed
-      noremap <Up> <Nop>
       noremap <Down> <Nop>
       noremap <Left> <Nop>
       noremap <Right> <Nop>
+      noremap <Up> <Nop>
+      set clipboard=unnamed
+      set expandtab
+      set number
+      set shiftwidth=2
+      set tabstop=2
       vnoremap <leader>64 y:let @+ = system('base64 --decode', @")<cr><esc>
       vnoremap <leader><leader>64 y:echo system('base64 --decode', @")<cr>
     '';
