543f8be5b229bcee74309fb31287813ad3d35174
Denis Lavrushko <dlavrushko@protonmail.com>
2023-10-20 11:32:59 +0200

    feat(nvim): add hotkeys

--- apps/configs/neovim/lua/config/keymaps.lua
+++ apps/configs/neovim/lua/config/keymaps.lua
@@ -27,3 +27,40 @@ map("n", "<leader>fi", "<cmd>Telescope find_files no_ignore=true<cr>", { desc = 
 -- decode base64 on-the-fly and show or decode and add to the buffer
 vim.keymap.set("v", "<leader>64", "y:let @+ = system('base64 --decode', @\")<cr><esc>")
 vim.keymap.set("v", "<leader><leader>64", "y:echo system('base64 --decode', @\")<cr>")
+
+-- LSP HotKeys
+map("n", "gr", vim.lsp.buf.references, { desc = "GoTo references" })
+map("n", "gd", vim.lsp.buf.definition, { desc = "GoTo definition" })
+map("n", "gD", vim.lsp.buf.declaration, { desc = "GoTo declaration" })
+map("n", "K", vim.lsp.buf.hover, { desc = "GoTo hover" })
+map("n", "gi", vim.lsp.buf.implementation, { desc = "GoTo implementation" })
+map("n", "<C-h>", vim.lsp.buf.signature_help, { desc = "GoTo signature_help" })
+map("n", "<leader>D", vim.lsp.buf.type_definition, { desc = "GoTo type_definition" })
+map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "GoTo rename" })
+map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "GoTo code_action" })
+map("v", "<leader>ca", vim.lsp.buf.code_action, { desc = "GoTo code_action" })
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>hh",
+  [[:lua require("harpoon.ui").toggle_quick_menu()<CR>]],
+  { noremap = true, silent = true }
+)
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>ha",
+  [[:lua require("harpoon.mark").add_file()<CR>]],
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>hn",
+  [[:lua require("harpoon.ui").nav_next()<CR>]],
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>hp",
+  [[:lua require("harpoon.ui").nav_prev()<CR>]],
+  { noremap = true, silent = true }
+)
