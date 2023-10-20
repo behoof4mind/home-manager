74a716795414a8ae16a7a5dc57c44403d5371452
Denis Lavrushko <dlavrushko@protonmail.com>
2023-10-20 11:32:26 +0200

    feat(all): add few plugins
    markdown, formatter, go-dep

--- apps/configs/neovim/lua/plugins/coding.lua
+++ apps/configs/neovim/lua/plugins/coding.lua
@@ -1,13 +1,55 @@
 return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    },
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+  {
+    "hashivim/vim-terraform",
+  },
+  {
+    "nvim-lua/plenary.nvim",
+    "ThePrimeagen/harpoon",
+  },
   { "tpope/vim-repeat", event = "VeryLazy" },
   {
     "towolf/vim-helm",
   },
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "gitignore",
+        "git_config",
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
+      })
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
       require("lspconfig").helm_ls.setup({})
+      require("lspconfig").gopls.setup({})
+      require("lspconfig").golangci_lint_ls.setup({})
+      require("lspconfig").bashls.setup({})
+      require("lspconfig").terraformls.setup({})
     end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
   },
   { "mfussenegger/nvim-dap" },
   {
@@ -16,9 +58,8 @@ return {
       require("dap-go").setup({})
     end,
   },
-
   {
-    "rcarriga/nvim-dap-ui",
+    "fatih/vim-go",
   },
   {
     "kylechui/nvim-surround",
