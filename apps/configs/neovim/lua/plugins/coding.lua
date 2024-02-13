06f277f248f2f91500ee3e8a3043c7fe3f957349
Denis Lavrushko <dlavrushko@protonmail.com>
2024-02-13 10:52:10 +0100

    fix(nvim): markdown plugin

--- apps/configs/neovim/lua/plugins/coding.lua
+++ apps/configs/neovim/lua/plugins/coding.lua
@@ -34,9 +34,8 @@ return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
+    build = "cd app && npm install",
+    lazy = true,
   },
   {
     "hashivim/vim-terraform",
