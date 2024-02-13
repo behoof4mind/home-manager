bcce28f97b45e4ce1110a7bb93b10aa0d44076c8
Denis Lavrushko <dlavrushko@protonmail.com>
2024-02-13 10:51:06 +0100

    fix(nvim): telescope grep

--- apps/configs/neovim/lua/plugins/coding.lua
+++ apps/configs/neovim/lua/plugins/coding.lua
@@ -8,6 +8,23 @@ return {
     },
     config = function()
       require("telescope").load_extension("yaml_schema")
+      require("telescope").setup({
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+          live_grep = {
+            additional_args = function()
+              return { "--hidden", "--glob", "!**/.git/*" }
+            end,
+          },
+          grep_string = {
+            additional_args = function()
+              return { "--hidden", "--glob", "!**/.git/*" }
+            end,
+          },
+        },
+      })
     end,
   },
   {
