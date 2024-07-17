8ed657cb38e4a9f5546633b64ca0bdd1a11d7790
Denis Lavrushko <dlavrushko@protonmail.com>
2024-07-17 23:25:29 +0200

    feat(neovim): add detailed view option to the oil

--- apps-configs/neovim/lua/plugins/file-editor.lua
+++ apps-configs/neovim/lua/plugins/file-editor.lua
@@ -5,5 +5,22 @@ return {
       show_hidden = true,
     },
   },
+  config = function()
+    require("oil").setup({
+      keymaps = {
+        ["gd"] = {
+          desc = "Toggle file detail view",
+          callback = function()
+            detail = not detail
+            if detail then
+              require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+            else
+              require("oil").set_columns({ "icon" })
+            end
+          end,
+        },
+      },
+    })
+  end,
   dependencies = { "nvim-tree/nvim-web-devicons" },
 }
