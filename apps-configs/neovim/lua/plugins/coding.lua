775aa5386802722267223d41942179dd87429da7
Denis Lavrushko <dlavrushko@protonmail.com>
2024-07-22 12:28:24 +0200

    refactor(neovim): move LSP outside of the coding.lua

--- apps-configs/neovim/lua/plugins/coding.lua
+++ apps-configs/neovim/lua/plugins/coding.lua
@@ -42,37 +42,6 @@ return {
     end,
   },
   {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("lspconfig").yamlls.setup({
-        on_attach = function(client, bufnr)
-          local excluded_patterns = { ".+/templates/.+.yaml", ".+/templates/.+%.tpl", ".+%.gotmpl", "helmfile.+%.yaml" }
-          local filename = vim.fn.expand("%:p")
-          for _, pattern in ipairs(excluded_patterns) do
-            if string.match(filename, pattern) then
-              vim.lsp.stop_client(client.id)
-            end
-          end
-        end,
-      })
-      require("lspconfig").bashls.setup({})
-      require("lspconfig").golangci_lint_ls.setup({})
-      require("lspconfig").gopls.setup({})
-      require("lspconfig").helm_ls.setup({})
-      require("lspconfig").jsonls.setup({})
-      require("lspconfig").lua_ls.setup({})
-      require("lspconfig").marksman.setup({})
-      require("lspconfig").terraformls.setup({})
-      require("lspconfig").nil_ls.setup({})
-      require("lspconfig").vuels.setup({})
-      require("lspconfig").dockerls.setup({})
-      require("lspconfig").docker_compose_language_service.setup({})
-      require("lspconfig").ansiblels.setup({})
-      require("lspconfig").sqlls.setup({})
-      require("lspconfig").pylsp.setup({})
-    end,
-  },
-  {
     "hrsh7th/nvim-cmp",
     "hrsh7th/cmp-nvim-lsp",
   },
--- apps-configs/neovim/lua/plugins/lsp.lua
+++ apps-configs/neovim/lua/plugins/lsp.lua
@@ -1,3 +1,35 @@
 return {
   { import = "plugins.lang" },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("lspconfig").yamlls.setup({
+        on_attach = function(client, bufnr)
+          local excluded_patterns = { ".+/templates/.+.yaml", ".+/templates/.+%.tpl", ".+%.gotmpl", "helmfile.+%.yaml" }
+          local filename = vim.fn.expand("%:p")
+          for _, pattern in ipairs(excluded_patterns) do
+            if string.match(filename, pattern) then
+              vim.lsp.stop_client(client.id)
+            end
+          end
+        end,
+      })
+      require("lspconfig").bashls.setup({})
+      require("lspconfig").golangci_lint_ls.setup({})
+      require("lspconfig").gopls.setup({})
+      require("lspconfig").helm_ls.setup({})
+      require("lspconfig").html.setup({})
+      require("lspconfig").jsonls.setup({})
+      require("lspconfig").lua_ls.setup({})
+      require("lspconfig").marksman.setup({})
+      require("lspconfig").terraformls.setup({})
+      require("lspconfig").nil_ls.setup({})
+      require("lspconfig").vuels.setup({})
+      require("lspconfig").dockerls.setup({})
+      require("lspconfig").docker_compose_language_service.setup({})
+      require("lspconfig").ansiblels.setup({})
+      require("lspconfig").sqlls.setup({})
+      require("lspconfig").pylsp.setup({})
+    end,
+  },
 }
