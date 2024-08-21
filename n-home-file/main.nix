3bc62c3bad5d1321e5b28cace399274ef72d5a86
Denis Lavrushko <dlavrushko@protonmail.com>
2024-08-21 23:38:19 +0200

    chore(iterm): fix dynamic profiles and update the config

--- n-home-file/main.nix
+++ n-home-file/main.nix
@@ -7,6 +7,8 @@ in
 {
   "${XDG_PATH}/cz/config.yaml".source = ../apps-configs/git/commitizen-config.yaml;
   "${XDG_PATH}/iterm2/DynamicProfiles/iterm-profiles.json".source = ../apps-configs/iterm2-profiles.json;
+  "${builtins.getEnv "HOME"}/Library/Application\ Support/iTerm2/DynamicProfiles/iterm2/DynamicProfiles/iterm-profile.json".source = ../apps-configs/iterm2-profile.json;
+  "${builtins.getEnv "HOME"}/Library/Preferences/com.googlecode.iterm2.plist".source = ../apps-configs/iterm2-config.plist;
   "${builtins.getEnv "HOME"}/.config/karabiner/karabiner.json".source = ../apps-configs/karabiner.json;
   "${builtins.getEnv "HOME"}/.ctags".source = ../apps-configs/.ctags;
   "${builtins.getEnv "HOME"}/.gitconfig-ftapi-github".source = ../apps-configs/git/.gitconfig-ftapi-github;
